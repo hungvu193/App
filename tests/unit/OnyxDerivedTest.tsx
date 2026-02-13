7f575f284fd50f323aa48a46fb1034e585f5897f
Hans <visaocuoitroi193@gmail.com>
2026-02-13 14:25:13 +0700

    prettier

--- tests/unit/OnyxDerivedTest.tsx
+++ tests/unit/OnyxDerivedTest.tsx
@@ -137,20 +137,17 @@ describe('OnyxDerived', () => {
 
             // When the report attributes are recomputed with both report and transaction updates
             reportAttributes.compute([reports, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined], {areAllConnectionsSet: true});
-            const reportAttributesComputedValue = reportAttributes.compute(
-                [reports, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined],
-                {
-                    sourceValues: {
-                        [ONYXKEYS.COLLECTION.REPORT]: {
-                            [`${ONYXKEYS.COLLECTION.REPORT}${reportID1}`]: reports[`${ONYXKEYS.COLLECTION.REPORT}${reportID1}`],
-                        },
-                        [ONYXKEYS.COLLECTION.TRANSACTION]: {
-                            [`${ONYXKEYS.COLLECTION.TRANSACTION}${transaction.transactionID}`]: transaction,
-                        },
+            const reportAttributesComputedValue = reportAttributes.compute([reports, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined], {
+                sourceValues: {
+                    [ONYXKEYS.COLLECTION.REPORT]: {
+                        [`${ONYXKEYS.COLLECTION.REPORT}${reportID1}`]: reports[`${ONYXKEYS.COLLECTION.REPORT}${reportID1}`],
                     },
-                    areAllConnectionsSet: true,
+                    [ONYXKEYS.COLLECTION.TRANSACTION]: {
+                        [`${ONYXKEYS.COLLECTION.TRANSACTION}${transaction.transactionID}`]: transaction,
+                    },
                 },
-            ).reports;
+                areAllConnectionsSet: true,
+            }).reports;
 
             // Then the computed report attributes should contain both reports
             expect(Object.keys(reportAttributesComputedValue)).toEqual([reportID1, reportID2]);
